75026de6d643b8a18435a6bac7a5c74c34184396
Rangerson Clemente <rangerson14@outlook.com>
2023-12-11 07:14:19 -0300

    update

--- flutter_application_despesas/lib/main.dart
+++ flutter_application_despesas/lib/main.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
-import 'Home.dart';
 
-void main() {
-    runApp(MaterialApp(
-      home: Home(),
-    ));
+void main() => runApp(MyNotepad());
+
+class MyNotepad extends StatelessWidget{
+  @override
+
+  Widget build(BuildContext contx){
+    return MaterialApp(
+      title: 'Meu Bloco de Notas',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Meu Bloco de Notas'),
+        ),
+
+        body: Center(
+          child: Container(
+            child: Text('Notas vazias'),
+          ),
+        ),
+      ),
+    );
+  }
 }
-
