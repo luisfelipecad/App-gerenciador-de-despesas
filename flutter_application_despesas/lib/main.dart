fd8ebfc8930d593fe9cb1e2660f8d61caebd4355
Rangerson Clemente <rangerson14@outlook.com>
2023-12-12 23:22:04 -0300

    Update main.dart

--- flutter_application_despesas/lib/main.dart
+++ flutter_application_despesas/lib/main.dart
@@ -21,7 +21,7 @@ class NoteList extends StatefulWidget {
 }
 
 class _NoteListState extends State<NoteList> {
-  List<String> notes = [];
+  Map<String,String> notes = {};
 
   @override
   Widget build(BuildContext context) {
@@ -32,8 +32,17 @@ class _NoteListState extends State<NoteList> {
       body: ListView.builder(
         itemCount: notes.length,
         itemBuilder: (context, index) {
+          String titulo = notes.keys.elementAt(index);
+          String conteudo = notes.values.elementAt(index);
+
           return ListTile(
-            title: Text(notes[index]),
+            title: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(titulo),
+                Text(conteudo),
+              ],
+            ),
             onTap: () {
               _editNote(index);
             },
@@ -51,71 +60,136 @@ class _NoteListState extends State<NoteList> {
   }
 
   void _addNote() {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        TextEditingController _titulo = TextEditingController();
-        TextEditingController _conteudo = TextEditingController();
-        return AlertDialog(
-          title: Text('Adicionar Nota'),
-          content: TextFormField(
-            controller: _titulo,
-            decoration: InputDecoration(labelText: 'Titulo'),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('Cancelar'),
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      TextEditingController _titulo = TextEditingController();
+      TextEditingController _conteudo = TextEditingController();
+      return AlertDialog(
+        content: Container(
+          padding: EdgeInsets.symmetric(horizontal: 15, vertical: 30),
+          color: const Color.fromARGB(255, 44, 105, 46),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: _titulo,
+                  decoration: const InputDecoration(labelText: 'Titulo'),
+                  validator: (String? value) {
+                    if (value!.isEmpty) {
+                      return 'Campo obrigatório';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _conteudo,
+                  decoration: const InputDecoration(labelText: 'Conteudo'),
+                  validator: (String? value) {
+                    if (value!.isEmpty) {
+                      return 'Campo obrigatório';
+                    }
+                    return null;
+                  },
+                  maxLines: 10,
+                ),
+                const SizedBox(height: 20),
+              ],
             ),
-            TextButton(
-              onPressed: () {
+          ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('Cancelar'),
+          ),
+          TextButton(
+            onPressed: () {
+              if (_titulo.text.isNotEmpty && _conteudo.text.isNotEmpty) {
                 setState(() {
-                  notes.add(_titulo.text);
+                  notes[_titulo.text, _conteudo.text] = _titulo.text;
+                  notes[_conteudo.text] = _conteudo.text;
                 });
                 Navigator.of(context).pop();
-              },
-              child: Text('Adicionar'),
-            ),
-          ],
-        );
-      },
-    );
-  }
+              }
+            },
+            child: Text('Adicionar'),
+          ),
+        ],
+      );
+    },
+  );
+}
+
 
   void _editNote(int index) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        TextEditingController _controller = TextEditingController(text: notes[index]);
-        return AlertDialog(
-          title: Text('Editar Nota'),
-          content: TextField(
-            controller: _controller,
-            decoration: InputDecoration(labelText: 'Conteúdo da Nota'),
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      String tituloAtual = notes.keys.elementAt(index);
+      String conteudoAtual = notes.values.elementAt(index);
+
+      TextEditingController _titulo = TextEditingController(text: tituloAtual);
+      TextEditingController _conteudo = TextEditingController(text: conteudoAtual);
+
+      return AlertDialog(
+        content: Container(
+          padding: EdgeInsets.symmetric(horizontal: 15, vertical: 30),
+          color: Color.fromARGB(255, 71, 90, 143),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: _titulo,
+                  decoration: const InputDecoration(labelText: 'Titulo'),
+                  validator: (String? value) {
+                    if (value!.isEmpty) {
+                      return 'Campo obrigatório';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _conteudo,
+                  decoration: const InputDecoration(labelText: 'Conteudo'),
+                  validator: (String? value) {
+                    if (value!.isEmpty) {
+                      return 'Campo obrigatório';
+                    }
+                    return null;
+                  },
+                  maxLines: 10,
+                ),
+                const SizedBox(height: 20),
+              ],
+            ),
           ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('Cancelar'),
-            ),
-            TextButton(
-              onPressed: () {
-                setState(() {
-                  notes[index] = _controller.text;
-                });
-                Navigator.of(context).pop();
-              },
-              child: Text('Salvar'),
-            ),
-          ],
-        );
-      },
-    );
-  }
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('Cancelar'),
+          ),
+          TextButton(
+            onPressed: () {
+              setState(() {
+                notes.remove(tituloAtual);
+                notes[_titulo.text] = _conteudo.text;
+              });
+              Navigator.of(context).pop();
+            },
+            child: Text('Salvar'),
+          ),
+        ],
+      );
+    },
+  );
+}
+
 
   void _deleteNote(int index) {
     showDialog(
@@ -134,7 +208,7 @@ class _NoteListState extends State<NoteList> {
             TextButton(
               onPressed: () {
                 setState(() {
-                  notes.removeAt(index);
+                  notes.remove(index);
                 });
                 Navigator.of(context).pop();
               },
