12e4bb8931e4f5381cf9d50e9ccfa4e93512d193
Rangerson Clemente <rangerson14@outlook.com>
2023-12-13 17:54:42 -0300

    Update main.dart

--- flutter_application_despesas/lib/main.dart
+++ flutter_application_despesas/lib/main.dart
@@ -10,7 +10,7 @@ class NoteApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Blocos de Notas',
-      home: NoteList(),
+      home: _LoginNotas(),
     );
   }
 }
